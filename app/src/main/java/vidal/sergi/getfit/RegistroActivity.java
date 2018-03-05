6a3a36454ef73d23b93f693f9afc823a60675ff6
Sergi Vidal <svidalmestre@gmail.com>
2018-03-05 20:35:07 +0100

    Implement users registration

---
+++ app/src/main/java/vidal/sergi/getfit/RegistroActivity.java
@@ -0,0 +1,57 @@
+package vidal.sergi.getfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btnRegistrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnRegistrar = findViewById(R.id.registrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("BUTTON","onCLick()");
+                String emailRegistro = email.getText().toString();
+                String passwordRegistro = password.getText().toString();
+                registrar(emailRegistro, passwordRegistro);
+            }
+        });
+    }
+
+    private void registrar(String email, String pass){
+        Log.d("SESION", "registrar()");
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    //Intent intent = new Intent(RegistroActivity.this, LoginActivity.class);
+                    //startActivity(intent);
+                    Log.d("SESION", "Usuario creado correctamente");
+                }else {
+                    Log.d("SESION", task.getException().getMessage()+"");
+                }
+            }
+        });
+    }
+}
